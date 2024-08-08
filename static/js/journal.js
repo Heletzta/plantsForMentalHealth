c72e51a7ca244463f7ade178bc803a57836ad062
heletzta <heletz.tami@gmail.com>
2024-08-07 18:22:45 -0700

    events.json file, and rearranging templates

--- static/js/journal.js
+++ static/js/journal.js
@@ -3,7 +3,6 @@ function submitted() {
         document.getElementById("status").textContent = "You did it! It's awesome that you wrote about your day today!";
     } else if (document.getElementById("entered").value == '2') {
         document.getElementById("status").textContent = "there was an error in submitting your form. Please try again.";
-
     }
 
 }
